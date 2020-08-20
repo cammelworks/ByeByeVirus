4fbb3d289ec3e4fe0b110a2df4732c00b2a64954
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-08-20 21:26:35 +0900

    スタートボタンを押すと画像を表示する

--- docs/src/main.js
+++ docs/src/main.js
@@ -1,11 +1,20 @@
-new Vue({
-  el: "#start",
+var title = new Vue({
+  el: "#title",
   data:{
     seen: true
   },
   methods:{
     toggleTitle: function(){
       this.seen = !this.seen
+      game.seen = !game.seen
     }
+  }
+})
+
+var game = new Vue({
+  el: "#game",
+  data: {
+    seen: false,
+    image: "../figs/virus_corona.png"
   }
 })
